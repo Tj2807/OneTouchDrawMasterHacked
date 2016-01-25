34a3b0c2f9a3a8c39eb5d3d5e74c6026cd17c3bb
Arpan Majumdar <Arpan Majumdar>
2016-01-25 15:06:00 +0530

    Output image will display labelled nodes, hence easy for testing

---
+++ showAnnotatedImg.m
@@ -0,0 +1,18 @@
+function showAnnotatedImg( img_crop,noNode,Ccentroid )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+text_str = cell(noNode,1);
+
+for k=1:noNode
+   text_str{k} = num2str(k);
+end
+
+position = floor(Ccentroid);
+box_color = {'red'};
+%RGB = insertText(I,position,text_str,'FontSize',18,'BoxColor',box_color,'BoxOpacity',0.4,'TextColor','white');
+annotatedImg = insertText(img_crop,position,text_str,'FontSize',50,'BoxColor',box_color,'TextColor','white');
+figure(4)
+imshow(annotatedImg)
+
+end
+
